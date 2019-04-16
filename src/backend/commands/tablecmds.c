2d22e6b09bbea2e377f1bfd844d34c172d1643d1
Takuma Hoshiai <hoshiai@sraoss.co.jp>
2019-04-16 10:59:27 +0900

    Add CREATE MATERIALIZED VIEW option and relisivm column in pg_class
    New option is used as follows:
     CREATE [INCREMANTAL] MATERIALIZED VIEW xxxxx AS SELECT ....;
    
    And if materialized view is created by above setting, new column in pg_class, relisivm is true.

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -866,6 +866,7 @@ DefineRelation(CreateStmt *stmt, char relkind, Oid ownerId,
 										  allowSystemTableMods,
 										  false,
 										  InvalidOid,
+										  stmt->ivm,
 										  typaddress);
 
 	/*
--- src/include/catalog/heap.h
+++ src/include/catalog/heap.h
@@ -80,6 +80,7 @@ extern Oid heap_create_with_catalog(const char *relname,
 						 bool allow_system_table_mods,
 						 bool is_internal,
 						 Oid relrewrite,
+						 bool relisivm,
 						 ObjectAddress *typaddress);
 
 extern void heap_drop_with_catalog(Oid relid);
