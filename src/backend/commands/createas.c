f6347132c009650f78a8b6ea4e0ed7b1aad5e382
thoshiai <45982834+thoshiai@users.noreply.github.com>
2020-05-01 17:16:15 +0900

    Fix log hint message (#100)
    One error log when creating a immv has been corrected to a more
    accurate representation.

--- src/backend/commands/createas.c
+++ src/backend/commands/createas.c
@@ -1287,7 +1287,7 @@ check_ivm_restriction_walker(Node *node, check_ivm_restriction_context *ctx, int
 					ereport(ERROR,
 							(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
 							 errmsg("this query is not allowed on incrementally maintainable materialized view"),
-							 errhint("WHERE clause only support subquery with EXISTS clause")));
+							 errhint("subquery in WHERE clause only supports subquery with EXISTS clause")));
 				if (depth > 0)
 					ereport(ERROR,
 							(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
