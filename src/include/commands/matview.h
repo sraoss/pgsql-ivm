2cdd67e1be61b853dc297a4b031e88141a7a9f99
Yugo Nagata <nagata@sraoss.co.jp>
2022-09-05 15:55:01 +0900

    Use snapshot to check tuple visibility in pre-update state
    When multiple tables are updated or the view contains a self-join,
    we need to calculate table states that was before it is modified
    during incremental view maintenance. For get the pre-update state,
    tuples inserted in a command must be removed when the table is scanned.
    
    Previously, we used xmin and cmin system columns for this purpose,
    but this way is problematic because after a tuple is frozen, its xmin
    no longer has any meaning. Actually, we will get inconsistent view
    results after XID wraparound.
    
    Also, we can see similar similar inconsistency when using sub-transaction
    because xmin values do not always monotonically increasing by command
    executions.
    
    To fix this, we use a snapshot that taken just before the table is
    modified for checking tuple visibility in pre-state table instead of
    using xmin and cmin system columns. A new function returning boolean,
    ivm_visible_in_prestate, is added, and this is called in WHERE clause
    of sub-queries to calculate pre-state table. This function check if a
    specified tuple in the post-update table is visible or not using the
    snapshot and return true if the tuple is visible.
    
    Github issue #145

--- src/include/commands/matview.h
+++ src/include/commands/matview.h
@@ -35,6 +35,7 @@ extern bool MatViewIncrementalMaintenanceIsEnabled(void);
 
 extern Datum IVM_immediate_before(PG_FUNCTION_ARGS);
 extern Datum IVM_immediate_maintenance(PG_FUNCTION_ARGS);
+extern Datum IVM_visible_in_prestate(PG_FUNCTION_ARGS);
 extern Query* rewrite_query_for_exists_subquery(Query *query);
 extern void AtAbort_IVM(void);
 extern char *getColumnNameStartWith(RangeTblEntry *rte, char *str, int *attnum);
